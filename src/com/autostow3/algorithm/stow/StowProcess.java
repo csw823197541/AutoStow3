77c480a4d1c67af0f4e4a4f6d379c246b3ce4fc7
csw823197541 <823917541@qq.com>
2018-11-20 18:00:35 +0800

    1

--- src/com/autostow3/algorithm/stow/StowProcess.java
+++ src/com/autostow3/algorithm/stow/StowProcess.java
@@ -45,31 +45,28 @@ public class StowProcess {
 
         List<StowMove> stowMoveList = stowAnalyzer.changeToStowMove(vesselContainers);
 
-        // 检查配载结果是否合理，主要检查槽重，进行调整
-        while (!checkAndChangeStowResult(stowMoveList, workingData)) {
+        // 检查配载结果是否合理，主要检查槽重，进行配载结果调整
+        while (!checkAndAdjustStowResult(stowMoveList, workingData)) {
             // 2.1 预配位按时间排序
             // 2.2 选择候选箱：依据berthId、groupId、weightId选择候选箱
-            // 2.2 根据箱区16分钟出4箱能力筛选能出箱的箱子，考虑到双箱吊出箱的情况
+            // 2.2 根据箱区16分钟出4箱能力筛选能出箱的箱子，考虑到双箱吊出箱的特殊情况
             // 2.3 对候选箱子进行打分：
             //      计算翻箱时间+
-            //      AGV和ASC消耗的时间+
-            //      计算initialTime，箱子到达桥机的时间点于CWP计划作业时间点的差值，（绝对值越小越接近计划作业时间）
-            //      同排在场箱配到船上同一个舱里面的分值
+            //      计算AGV和ASC消耗的时间+
+            //      计算initialTime，箱子到达桥机的时间点与CWP计划作业时间点的差值，（绝对值越小越接近计划作业时间）
+            //      计算同排在场箱配到船上同一个舱里面的分值
             // 2.4 选择分值最合适的箱子作为配载结果，深度和宽度搜索最优配载箱
-
             // 2.5 更新箱区出箱能力值
 
             // 2.6 所有箱子配完，检查超重负荷，先计算槽重正负差值，同属性组箱子交换，交换后箱子不能超过槽重值
             // 2.7 重新执行配载，没有超过槽重值的情况结束配载，得到最优结果
-
-            // 3、重压轻调整
         }
 
         long et = System.currentTimeMillis();
         logger.logInfo("执行结束，执行时间是：" + (et - st) / 1000 + "秒");
     }
 
-    private boolean checkAndChangeStowResult(List<StowMove> stowMoveList, WorkingData workingData) {
+    private boolean checkAndAdjustStowResult(List<StowMove> stowMoveList, WorkingData workingData) {
         // 取每根槽的船箱位对应配载结果，计算配载箱子的重量，与槽重进行对比，检查是否超过槽重
 
         // 1、超过槽重的依据“交换原则”调整船箱位对应的配载结果
